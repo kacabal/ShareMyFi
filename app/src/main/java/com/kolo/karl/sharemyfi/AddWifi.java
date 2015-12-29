d54d9c24e438c2a6a60c61afa20e40b1796ea228
Karl Acabal <kacabal@gmail.com>
2015-12-29 17:01:00 -0500

    Add StorageUtil for interacting with stored credentials

--- app/src/main/java/com/kolo/karl/sharemyfi/AddWifi.java
+++ app/src/main/java/com/kolo/karl/sharemyfi/AddWifi.java
@@ -2,15 +2,17 @@ package com.kolo.karl.sharemyfi;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddWifi extends AppCompatActivity {
 
     public static String ADD_NEW_WIFI_INFO = "com.kolo.karl.sharemyfi.AddNewWifiInfo";
+    public static final String TAG = "AddWifi";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +22,22 @@ public class AddWifi extends AppCompatActivity {
         setSupportActionBar(toolbar);
         toolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.black));
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.ID_ACTION_SAVE_WIFI_INFO);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                EditText ssidInput = (EditText)findViewById(R.id.ID_SSID_INPUT);
+                EditText passInput = (EditText)findViewById(R.id.ID_PASS_INPUT);
+
+                StorageUtil storageUtil = new StorageUtil();
+                if (storageUtil.addWifiInfo(ssidInput.getText().toString(),
+                        passInput.getText().toString()) == StorageUtil.ADDED_OK)
+                {
+                    String toastMsg = getString(R.string.TXT_ADDED_COLON) + " " + ssidInput.getText();
+                    Toast.makeText(AddWifi.this, toastMsg, Toast.LENGTH_SHORT).show();
+                }
+
+                finish();
             }
         });
     }
---
+++ app/src/main/java/com/kolo/karl/sharemyfi/StorageUtil.java
@@ -0,0 +1,15 @@
+package com.kolo.karl.sharemyfi;
+
+/**
+ * Created by karl on 12/29/15.
+ */
+public class StorageUtil
+{
+    public static final int ADDED_OK = 0;
+    public static final int ADDED_GENERIC_ERROR = -1;
+
+    public static int addWifiInfo(String ssid, String pass)
+    {
+        return ADDED_OK;
+    }
+}
